e33fa136297842d3938f6e4923e316273ba3db58
mustafajam <mustafajamalshabqadar@gmail.com>
2025-07-06 16:26:37 +0500

    update app.js

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const userModel = require('./models/user');
 const Path=require('path');
 const user = require('./models/user');
 const { prototype } = require('events');
+import { fileURLToPath } from "url";
+
+// Vercel requires ES Modules, so we get __dirname this way
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.static(Path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.get('/',(req,res)=>{
